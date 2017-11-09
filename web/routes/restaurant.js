f3146865d7656114740183923fd09472e877f4c3
KangSangYeon <ksyline1997@gmail.com>
2017-11-09 21:15:12 +0900

    메뉴 관련 서버 처리 별도 스크립트로 분리

--- web/routes/restaurant.js
+++ web/routes/restaurant.js
@@ -137,48 +137,4 @@ router.get('/list/', (req, res)=>{
 });
 
 
-/**
- * 레스토랑 메뉴 정보 요청
- */
-router.get('/menu/:menu_seq/', (req, res)=>{
-	var menu_seq = req.params.menu_seq;
-
-	var sql_select = "SELECT * " +
-		"FROM restaurant_menu " +
-		"WHERE menu_seq=?";
-	db.get().query(sql_select, [menu_seq], (err, rows)=>{
-		console.log("sql_select : " + sql_select);
-		if(err) {
-			console.log(err.message);
-			return res.sendStatus(400);
-		}
-
-		res.contentType('application/json');
-		return res.status(200).send(JSON.stringify(rows[0], null, 4));
-	});
-});
-
-/**
- * 레스토랑 메뉴 정보 등록
- */
-router.post('/menu/', (req, res)=>{
-	var restaurant_seq = req.body.restaurant_seq;
-	var name = req.body.name;
-	var image = req.body.image;
-	var price = req.body.price;
-
-	var sql_insert = "INSERT INTO restaurant_menu(restaurant_seq, name, image, price) " +
-		"VALUES(?, ?, ?, ?);";
-	db.get().query(sql_insert, [restaurant_seq, name, image, price], (err, rows)=>{
-		console.log("sql_insert : " + sql_insert);
-		if(err) {
-			console.log(err.message);
-			return res.sendStatus(400);
-		}
-
-		res.contentType('application/json');
-		return res.status(200).send(JSON.stringify(rows, null, 4));
-	});
-});
-
 module.exports = router;
